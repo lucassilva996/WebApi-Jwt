dd74445b73b64749db6b29642da0d69bf6b5492a
lucassilva996 <lucassilva996@outlook.com>
2023-02-17 01:04:33 -0300

    Criando serviço que gera o token e a classe estatica onde fica o token default

---
+++ Services/TokenGeneratorService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using WebApi_Jwt.Models;
+using System.IdentityModel.Tokens;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+
+namespace WebApi_Jwt.Services
+{
+    public class TokenGeneratorService
+    {
+        public static string GenerateToken(User user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(Settings.Settings.Secret);
+            var tokenDescript = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.Username.ToString()),
+                    new Claim(ClaimTypes.Role, user.Role.ToString()),
+                }),
+                Expires = DateTime.UtcNow.AddHours(2),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescript);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
---
+++ Settings/Settings.cs
@@ -0,0 +1,7 @@
+﻿namespace WebApi_Jwt.Settings
+{
+    public static class Settings
+    {
+        public static string Secret = "296d088d5fe2c89dc81cb656cf444429";
+    }
+}
