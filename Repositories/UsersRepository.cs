31b70c222b4534d75583d7046bf0c24de02717e3
lucassilva996 <lucassilva996@outlook.com>
2023-02-17 00:43:50 -0300

    Adicionando a class de repositório

---
+++ Repositories/UsersRepository.cs
@@ -0,0 +1,22 @@
+﻿using WebApi_Jwt.Models;
+
+namespace WebApi_Jwt.Repositories
+{
+    public class UsersRepository
+    {
+        public static User Get(string username, string password)
+        {
+            var users = new List<User>();
+
+            users.Add(new User
+            {
+                Id = 1, 
+                Username = "lucassilva996",
+                Password = "@201013#",
+                Role = "manager"
+            });
+
+            return users.Where(x => x.Username.ToLower() == username && x.Password == x.Password).First();
+        }
+    }
+}
